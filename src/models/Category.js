7ee055a8565b26f9f0d05136582c6c9e63cd18d6
Vitor M. Alves <vtr.vma@hotmail.com>
2023-03-06 14:47:57 -0300

    Fix: ajuste no model Category

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,7 +1,8 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  const Categories = sequelize.define('Categories', {
+  const Categories = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: DataTypes.STRING
   },{
     tableName: 'categories',
